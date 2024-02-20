fab02320b46a69f5cc278a0adab2bbcc008502f1
Alex Franchuk <alex.franchuk@gmail.com>
2024-02-20 16:51:55 -0500

    Support multiline recipe prefixes.

--- conf/metadata.php
+++ conf/metadata.php
@@ -2,6 +2,6 @@
 
 $meta['compiler'] = array('string');
 $meta['compiler_flags'] = array('array');
-$meta['recipe_prefix'] = array('string');
+$meta['recipe_prefix'] = array('');
 $meta['scaling'] = array('multichoice', '_choices' => array('manual', 'compiler'));
 $meta['compiler_scaling_flag'] = array('string');
